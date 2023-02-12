6a3a300bb8b3b5a385429d2607221a0a839262cb
Erjan04 <theerjoinlast@gmail.com>
2023-02-12 22:37:15 +0300

    added clean arch package and connected permission,transelute windows and application super class

---
+++ app/src/main/java/com/example/appmarket/App.java
@@ -0,0 +1,6 @@
+package com.example.appmarket;
+
+import android.app.Application;
+
+public class App extends Application {
+}
--- app/src/main/java/com/example/appmarket/presentation/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/appmarket/presentation/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.appmarket;
+package com.example.appmarket.presentation.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.appmarket.R;
 
 public class MainActivity extends AppCompatActivity {
 
