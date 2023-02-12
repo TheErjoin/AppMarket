7a67dc924c9ba0d369a96c9b38bb2c70e2cdd1ca
Erjan04 <theerjoinlast@gmail.com>
2023-02-12 23:49:17 +0300

    added navigation dependency and fragment pattern by template

---
+++ app/src/main/java/com/example/appmarket/presentation/ui/fragments/apps/AppsFragment.java
@@ -0,0 +1,38 @@
+package com.example.appmarket.presentation.ui.fragments.apps;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.appmarket.R;
+
+public class AppsFragment extends Fragment {
+
+    private AppsViewModel mViewModel;
+
+    public static AppsFragment newInstance() {
+        return new AppsFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_apps, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(AppsViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/appmarket/presentation/ui/fragments/apps/detail/DetailAppViewModel.java
@@ -0,0 +1,7 @@
+package com.example.appmarket.presentation.ui.fragments.apps.detail;
+
+import androidx.lifecycle.ViewModel;
+
+public class DetailAppViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
