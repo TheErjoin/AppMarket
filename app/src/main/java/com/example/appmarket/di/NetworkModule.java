910b8a8fbebf1a6b832bc48dd0ebb458d32eb424
TheErjoin <theerjoinlast@gmail.com>
2023-02-14 02:40:44 +0300

    added di network module, and added baseurl

--- app/src/main/java/com/example/appmarket/di/NetworkModule.java
+++ app/src/main/java/com/example/appmarket/di/NetworkModule.java
@@ -1,4 +1,13 @@
 package com.example.appmarket.di;
 
+import dagger.Module;
+import dagger.hilt.InstallIn;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
 public class NetworkModule {
+
+
+
 }
